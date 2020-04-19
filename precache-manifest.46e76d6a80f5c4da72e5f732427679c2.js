187662f7d451b6feb148b3b7062f966cc83c4b20
Tarlan Karimli <terlankerimli@gmail.com>
2020-04-19 17:45:49 +0500

    Updates

---
+++ precache-manifest.46e76d6a80f5c4da72e5f732427679c2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1547380a0973594338bfb48586f51a1d",
+    "url": "/panel-admin/index.html"
+  },
+  {
+    "revision": "b922573284e324e980a7",
+    "url": "/panel-admin/static/css/main.645dce58.chunk.css"
+  },
+  {
+    "revision": "2a6e895669b3b03eff4f",
+    "url": "/panel-admin/static/js/2.74ecf997.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/panel-admin/static/js/2.74ecf997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b922573284e324e980a7",
+    "url": "/panel-admin/static/js/main.21ceb516.chunk.js"
+  },
+  {
+    "revision": "0388e0b79d3598ec4f88",
+    "url": "/panel-admin/static/js/runtime-main.bb9a0002.js"
+  },
+  {
+    "revision": "c3fe7f18c2a0839b64db8bff0c0f7f46",
+    "url": "/panel-admin/static/media/arrow.c3fe7f18.svg"
+  },
+  {
+    "revision": "28296cb2e8f9707ac56fbd6e68ab640b",
+    "url": "/panel-admin/static/media/down-arrow.28296cb2.svg"
+  },
+  {
+    "revision": "fa012faf3acda1bc7852f6b00a0e895f",
+    "url": "/panel-admin/static/media/icons8-delete.fa012faf.svg"
+  },
+  {
+    "revision": "2364885a3a5ee42b74b7034497fb4d16",
+    "url": "/panel-admin/static/media/icons8-edit.2364885a.svg"
+  },
+  {
+    "revision": "2e70f9a372ea40d932ddf454f6641562",
+    "url": "/panel-admin/static/media/icons8-plus.2e70f9a3.svg"
+  },
+  {
+    "revision": "b1c29feafa82f8642bd1aca4b61e5d49",
+    "url": "/panel-admin/static/media/upload.b1c29fea.svg"
+  }
+]);
